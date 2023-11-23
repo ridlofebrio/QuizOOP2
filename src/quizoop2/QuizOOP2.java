b546190dddf03f3e7b7b7f02196746c7f57afbf9
Febrio <muhridlo111@gmail.com>
2023-11-23 21:03:46 +0700

    first commit

---
+++ src/quizoop2/QuizOOP2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quizoop2;
+
+/**
+ *
+ * @author Febrio
+ */
+public class QuizOOP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
